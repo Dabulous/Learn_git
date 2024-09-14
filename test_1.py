2cefa9dd6bd567624ce22f1285f5b39e77f05ae3
Busola Dada <dada.busola@gmail.com>
2024-09-13 20:45:31 -0400

    This is a Switch Case implement in Python using Class

---
+++ test_1.py
@@ -0,0 +1,22 @@
+class Python_Switch:
+    def day(self, month):
+
+        default = "Incorrect day"
+
+        return getattr(self, 'case_' + str(month), lambda: default)()
+
+    def case_1(self):
+        return "Jan"
+
+    def case_2(self):
+        return "Feb"
+
+    def case_3(self):
+        return "Mar"
+
+
+my_switch = Python_Switch()
+
+print(my_switch.day(1))
+
+print(my_switch.day(3))
